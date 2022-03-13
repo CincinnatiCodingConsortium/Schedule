57314eac9ab677c8b3f990a3e60893708fb7067e
RandonSRProctor <RandyProctor@Gmail.com>
2022-03-13 16:05:21 -0400

    Confirmed connected to database :-) Also no longer logging env variables :-P

--- src/app.js
+++ src/app.js
@@ -10,8 +10,24 @@ export const app = async () => {
     // const schedule = await fetchUpcomingSchedule()
     // console.log(schedule)
     
-    console.log(process.env.NOTION_SCHEDULE_PLANNER_INTEGRATION_TOKEN)
-    console.log(process.env.NOTION_SCHEDULE_DATABASE_ID)
+    const NOTION_SCHEDULE_PLANNER_INTEGRATION_TOKEN = process.env.NOTION_SCHEDULE_PLANNER_INTEGRATION_TOKEN
+    const NOTION_SCHEDULE_DATABASE_ID = process.env.NOTION_SCHEDULE_DATABASE_ID
+
+
+    // Set up SDK Client with authorization
+    const notion = new Client({
+        auth: NOTION_SCHEDULE_PLANNER_INTEGRATION_TOKEN
+    })
+
+ 
+    // Create 
+    async function fetchDatabase() {
+        const databaseId = NOTION_SCHEDULE_DATABASE_ID;
+        const response = await notion.databases.retrieve({ database_id: databaseId });
+        console.log(response)
+    }
+
+    fetchDatabase()
 
     // COMMENTED OUT TO AVOID NON-STOP POSTS ANNOYING KORY DURING TESTING
     // const result = await postToDiscord(TEST_DISCORD_WEBHOOK, { content: testMessage })
