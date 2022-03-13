609906f5e42f8d0262286880a015e04864f1d625
RandonSRProctor <RandyProctor@Gmail.com>
2022-03-13 15:47:15 -0400

    Changed evnironment variable name to be more accurate.

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import { fetchUpcomingSchedule } from "./webAPI/scheduleDB"
 import { postToDiscord } from "./webAPI/discord/postToDiscord"
 import { testMessage } from "./messages"
 import { TEST_DISCORD_WEBHOOK } from "./constants"
+import { Client } from "@notionhq/client"
 
 export const app = async () => {
 
@@ -9,7 +10,7 @@ export const app = async () => {
     // const schedule = await fetchUpcomingSchedule()
     // console.log(schedule)
     
-    console.log(process.env.NOTION_SCHEDULE_PLANNER_INTEGRATION)
+    console.log(process.env.NOTION_SCHEDULE_PLANNER_INTEGRATION_TOKEN)
     console.log(process.env.NOTION_SCHEDULE_DATABASE_ID)
 
     // COMMENTED OUT TO AVOID NON-STOP POSTS ANNOYING KORY DURING TESTING
